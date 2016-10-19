533fddb65a95f909dfdbec87c7a019c8b152e5a5
Damien 'Lanza' BRUN <dev@lanza.fr>
2016-10-19 21:53:38 +0200

    My little demo

---
+++ js/mapping.js
@@ -0,0 +1,307 @@
+﻿Microsoft.Maps.Map.prototype.panTo = function( b )
+{
+	if( this.anchoredCenter == undefined )
+		this.anchoredCenter = this.getCenter();
+	
+	if( this.oldLocation == undefined )
+		this.oldLocation = this.anchoredCenter;
+	
+	if( b instanceof Microsoft.Maps.Location ) {
+		map.setView({ 
+			bounds: Microsoft.Maps.LocationRect.fromLocations(
+						[
+							b,
+							this.oldLocation,
+							this.anchoredCenter
+						]),
+			animate: true
+		});
+		
+		this.oldLocation = b;
+		
+	}
+}
+
+Microsoft.Maps.Map.prototype.zoomTo = function( b, double )
+{
+		
+	if( b instanceof Microsoft.Maps.Location ) {
+		map.setView({ 
+			center: b,
+			zoom: double ? 15 : 12,
+			animate: true
+		});
+		this.oldLocation = b;
+	}
+}
+
+
+// =========
+// = Image =
+// =========
+
+function Image( url ) {
+	this.url = url;
+}
+
+Image.prototype.href = function( p ) {
+	
+	var qs = [];
+	var querystring = "";
+	
+	if( p ) {
+	 	if( p.width ) 
+			qs.push( "w=" + p.width );
+		
+		if( p.height )
+			qs.push("h=" + p.height );
+			
+		if( p.color ) 
+			qs.push("color=" + p.color );
+			
+		if( p.crop )
+			qs.push("crop=true");
+			
+		if( qs.length > 0 )
+			querystring = "?" + qs.join("&");
+	}
+	return this.url + querystring; 
+}
+
+function Spot( SpotOptions ) {
+	Microsoft.Maps.Location.call( this, SpotOptions.latitude, SpotOptions.longitude );
+	this.ID = SpotOptions.ID;
+	this.title = SpotOptions.title;
+	this.photo = new Image( SpotOptions.photoUrl );
+	var zindex = Math.round((1-(this.latitude-47)) * 1000);
+	this.pin = new Microsoft.Maps.Pushpin(this, {text:"1", visible:true, zIndex: zindex });
+	this.bubble = new PowerBubble( this );
+
+	if( SpotOptions.map ) {
+		this.Map = SpotOptions.map;
+	}
+}
+
+
+Spot.prototype = new Microsoft.Maps.Location;
+
+Spot.prototype.get_x = function() {
+	var x = NaN;
+	
+	if( this.Map.getBounds().contains( this ) )
+		x =  Math.round( this.Map.tryLocationToPixel(this, Microsoft.Maps.PixelReference.control).x ); 
+	
+	return x;	
+}
+
+Spot.prototype.get_y = function() {
+	var y = NaN;
+	
+	if( this.Map.getBounds().contains( this ) )
+		y =  Math.round( this.Map.tryLocationToPixel(this, Microsoft.Maps.PixelReference.control).y ); 
+	
+	return y;
+		
+}
+
+Spot.prototype.findOnMap = function(map) {
+
+
+	if( map )
+		this.Map = map;
+
+	if( !this.Map.getBounds().contains(this) ) {
+		
+		this.Map.panTo(this);
+	}
+	
+
+}
+
+Spot.prototype.focusOnMap = function(map, double ) {
+	/* map.entities.clear(); 
+	map.entities.push(this.pin);*/
+	
+	if( map )
+		this.Map = map;
+
+	
+	this.Map.zoomTo(this, double);	
+}
+
+Spot.prototype.attachToMap = function( map ) {
+	this.Map = map;
+}
+
+Spot.prototype.listView = function() {
+	var root = document.createElement("figure");
+	root.className= "item";
+	
+	var img = document.createElement("img");
+	img.src = this.photo.href({ height: 87 });
+	img.alt = this.title;
+	
+	var link = document.createElement("a");
+	link.href = this.photo.href({ height: 87 });
+	
+	link.appendChild( img );
+	root.appendChild( link );
+	
+	var me = this;
+	$(root).mouseenter(function(){
+		me.findOnMap(me.Map);
+		me.bubble.highlight();
+	});
+	
+ 	$(root).mouseleave(function(){
+		me.bubble.normal();
+	});
+	
+	
+	$(root).click(function(e){
+		me.focusOnMap(me.Map);
+		e.preventDefault();
+	});
+
+	$(root).dblclick(function( e ){
+		me.focusOnMap(me.Map, true);
+		e.preventDefault();
+	});
+
+	return root;
+}
+
+// =================================================
+// = PowerBubbles : Des infoBox en SVG/VML animées =
+// =================================================
+
+// Needs raphael-min.js, jQuery & Microsoft Maps.
+
+function PowerBubble( Spot ) {
+	this.Spot = Spot;
+	this.id = "bubble_" + Spot.ID;
+	this.gravCenter = this.pt(
+		0,
+		0
+	);
+	this.origin = this.pt( 0,0 );
+	
+	Microsoft.Maps.Infobox.call( this, this.Spot, {
+			width: this.canvas.width, height: this.canvas.height, 
+			offset: new Microsoft.Maps.Point(-this.canvas.width/2, -this.canvas.height/2),
+			htmlContent: '<div id="' + this.id + '" class="bubble"></div>'  
+		});
+	
+};
+
+PowerBubble.prototype = new Microsoft.Maps.Infobox;
+
+PowerBubble.prototype.canvas = { width: 2000, height: 2000 };
+PowerBubble.prototype.rectSize = { width: 115, height: 85 };
+PowerBubble.prototype.pt = function(x, y) {
+	x = this.canvas.width / 2 + x;
+	y = this.canvas.height / 2 + y;
+	
+	return { x: x, y: y };
+}
+
+PowerBubble.prototype.init = function() {
+
+	
+	this.bubblediv = $("#" + this.id).get()[0];
+	this.paper = Raphael( this.bubblediv, this.canvas.width, this.canvas.height );
+	this.circle = this.paper.circle(this.origin.x, this.origin.y, 4);
+	this.circle.glow({color: "#FF017D" });
+	this.circle.attr("fill", "#FFFFFF");
+	this.circle.attr("stroke-width", "0");
+	
+	this.paper.setStart();
+	this.paper.rect(this.origin.x, this.origin.y, 0, 0, 0).attr("fill", "#716F70" ).attr("stroke-width", "10").attr("stroke", "#716F70");
+	this.paper.image( this.Spot.photo.href({width:105, height:85, color: "#716F70"}), this.origin.x, this.origin.y, 0 );	
+	this.rect = this.paper.setFinish();
+	
+ 	this.line = this.paper.path( "M"+ this.origin.x +"," +this.origin.y +"L"+ this.origin.x +","+this.origin.y );
+	this.line.attr("stroke-width", "7");
+	this.line.attr("stroke", "#716F70");
+	
+	this.rect.toFront();
+	
+	var me = this;
+	$(this.bubblediv).mouseenter(function(){
+		me.highlight();
+	});
+	
+	$(this.bubblediv).mouseleave(function(){
+		me.normal();
+	});
+}
+
+PowerBubble.prototype.getNode = function() {
+	var point = this.Spot.Map.tryLocationToPixel( this.Spot );
+	
+	return new Node( 
+		point.x + (this.gravCenter.x), 
+		point.y + (this.gravCenter.y), 
+		new Vector( 
+			(this.gravCenter.x - this.origin.x ),
+			(this.gravCenter.y - this.origin.x )
+		 ) );
+}
+
+PowerBubble.prototype.getNodeOrigin = function() {
+	var point = this.Spot.Map.tryLocationToPixel( this.Spot );
+	
+	return new Node(
+		point.x + this.origin.x, point.y + this.origin.x, null
+	);
+}
+
+PowerBubble.prototype.applyForce = function( force ) {
+	this.gravCenter.x += force.x;
+	this.gravCenter.y += force.y;
+	this.floatAround();
+	
+	$(this.bubblediv.parentNode.parentNode).css( "z-index", Math.round( 1000 - (new Vector( 
+			(this.gravCenter.x - this.origin.x ),
+			(this.gravCenter.y - this.origin.x )
+		 ).norm() *2) ) );
+}
+
+
+PowerBubble.prototype.floatAround = function() {
+	this.rect.attr({ x:this.gravCenter.x - this.rectSize.width / 2, y: this.gravCenter.y - this.rectSize.height / 2 });
+	this.line.attr({path: "M"+ this.origin.x +"," +this.origin.y +"L"+ this.gravCenter.x +","+this.gravCenter.y  });
+}
+
+PowerBubble.prototype.highlight = function() {
+	this.line.attr("stroke", "#FF017D");
+	this.rect.attr("stroke", "#FF017D");
+	this.circle.attr("fill", "#FF017D");
+}
+
+PowerBubble.prototype.normal = function() {
+	this.line.attr("stroke", "#716F70");
+	this.rect.attr("stroke", "#716F70");
+	this.circle.attr("fill", "#FFFFFF");
+}
+
+PowerBubble.prototype.plop = function() {
+	this.rectAppear = Raphael.animation( { x:this.gravCenter.x - this.rectSize.width / 2, y: this.gravCenter.y - this.rectSize.height / 2, width: this.rectSize.width , height: this.rectSize.height }, 500, "bounce" );
+	this.lineAppear = Raphael.animation( { path:  "M"+ this.origin.x +"," +this.origin.y +"L"+ this.gravCenter.x +","+this.gravCenter.y }, 500, "bounce" );
+	
+	this.rect.animate(this.rectAppear);
+	this.line.animateWith( this.rect, this.rectAppear, this.lineAppear );
+
+}
+
+PowerBubble.prototype.slurp = function() {
+	this.rectDisappear = Raphael.animation( { x: this.origin.x, y: this.origin.y, width: 0, height: 0, r:0}, 200, "<" );
+	this.lineDisappear = Raphael.animation( { path: "M"+ this.origin.x +"," +this.origin.y +"L"+ this.origin.x +","+this.origin.y }, 200, "<" );
+	this.rect.stop();
+	this.line.stop();
+	this.rect.animate( this.rectDisappear );
+	this.line.animateWith( this.rect, this.rectDisappear, this.lineDisappear );
+	/* this.circle.stop();
+	this.circle.attr("fill", "#FFFFFF");
+	this.circle.attr("r", 4); */
+}
---
+++ js/vector.js
@@ -0,0 +1,162 @@
+﻿// ======================
+// = Calcul de vecteurs =
+// ======================
+
+var K = 250;
+function Vector( deltaX, deltaY ) {
+	this.x = deltaX;
+	this.y = deltaY;
+} 
+
+Vector.prototype = {
+	x: 0,
+	y: 0,
+	opposite: function() {
+		return new Vector( -this.x, -this.y );
+	},
+ 	add: function( v ) {
+ 		return new Vector( this.x + v.x, this.y + v.y );
+ 	},
+ 	substract: function( v ) {
+ 		var neg = v.opposite();
+ 		return this.add( neg );
+ 	},
+ 	multiply: function( a ) {
+ 		return new Vector( this.x * a, this.y * a );
+ 	},
+ 	norm: function() {
+ 		return Math.sqrt( (this.x * this.x) + (this.y * this.y) );
+ 	},
+ 	normalize: function() {
+ 		var n = this.norm();
+ 		if( n > 0 )
+ 			return new Vector( this.x / n, this.y / n );
+ 		else
+ 			return new Vector( 0,0 );
+ 	}
+};
+
+
+// =======================
+// = Point               =
+// =======================
+
+function Point( x, y ) {
+	this.x = x;
+	this.y = y;
+}
+
+Point.prototype = {
+	vectorTo: function( point ) {
+		return new Vector( point.x - this.x, point.y - this.y );
+	} 
+}
+
+// =======================
+// = GraphNode           =
+// =======================
+
+function Node( x, y, spring ) {
+	Point.call( this, x, y );
+	this.spring = spring;
+	this.direction = new Vector(0,0);
+	this.speed = new Vector( 0, 0 );
+}
+
+Node.prototype = new Point;
+
+// Fonction de répulsion.
+Node.prototype.repulse = function( node, coef ) {
+	coef = coef || 1;
+	var distance = node.vectorTo(this);
+	var dist = distance.norm();
+	dist = dist > K ? 0 : dist;
+	if( dist > 0 ) {
+		var strength =  -dist * (( Math.cos( dist / K * Math.PI ) + 1) );
+			
+		node.direction = node.direction.add( 
+			distance.normalize().multiply( strength *coef )
+		);
+	}
+	return node.direction;
+}
+
+Node.prototype.attract = function() {
+	var distance = this.spring;
+	var dist = distance.norm();
+	this.direction = this.direction.substract( 
+		distance.normalize().multiply( (dist*dist) / K )
+	);
+	 
+	 return this.direction;
+}
+
+// =======================================
+// = Node Set. Nodes couramment affichés =
+// =======================================
+
+
+function NodeSet() {
+	this.index = 0;
+}
+
+NodeSet.prototype = new Array;
+NodeSet.prototype.ratataPlop = function(me) {
+	if( me == undefined ) me = this; // setTimeOut assigne l'objet "window" à "this", donc on sauve "this" ailleurs.
+
+	if( me.index == me.length )
+		me.attractionRepulsion();
+	else
+		me[me.index].plop();
+
+	if( me.index < me.length ) {
+		me.index += 1;
+		setTimeout( function(){ me.ratataPlop(me) }, 100 ); // delayed recursivity. Love that.
+	}
+}
+
+NodeSet.prototype.attractionRepulsion = function( me ) {
+	if( me == undefined ) me = this; // setTimeOut assigne l'objet "window" à "this", donc on sauve "this" ailleurs.
+
+	if( this.temperature == undefined )
+		this.temperature = 5;
+
+	var nodes = new Array(me.length);
+	var origins = new Array(me.length);
+	
+	for( var i = 0; i< me.length; i++) {
+		nodes[i] = nodes[i] || me[i].getNode();
+		for( var j = 0; j < me.length; j++ ) {
+			if( i != j ) {
+				// histoire de ne pas masquer les points.
+				origins[j] = origins[j] || me[j].getNodeOrigin(); 
+				origins[j].repulse(nodes[i], 0.2);
+				
+				nodes[j] = nodes[j] || me[j].getNode();
+				nodes[j].repulse(nodes[i]);				
+				// les positions géo des spots repoussent les bulles qui ne leur appartiennent pas.
+
+			}
+		}
+		nodes[i].attract();
+	}
+	
+	for( var i = me.length-1; i >=0; i-=2 ) {
+		//
+	}
+	
+	for( var i = 0; i < me.length; i ++ )
+			me[i].applyForce( nodes[i].direction.multiply( 0.25 ) );
+	
+	if( this.temperature > 0 ) {
+		this.temperature -= 0.035; 
+		
+	} else {
+		this.temperature = 0;
+	}
+		
+	setTimeout( function(){ me.attractionRepulsion(me) }, 40 );
+}
+
+
+
